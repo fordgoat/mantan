920aeb9623b696ee89cb1f634cd18ab0999050f9
fordgoat <adaadaajapan@gmail.com>
2024-10-21 10:33:46 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,18 +14,18 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text = f"""
 𝗣𝗥𝗜𝗖𝗘 𝗟𝗜𝗦𝗧 𝗩𝗩𝗜𝗣 💰
 
-— VVIP INDO : Rp. 30.000,-
-— VVIP HIJAB : Rp. 35.000,-
-— VVIP ONLYFANS : Rp. 35.000,-
-— VVIP CAMPURAN : Rp. 30.000,-
-— VVIP JAV HD : Rp. 35.000,-
-— VVIP LIVE RECORD : Rp. 30.000,-
+— VVIP INDO : Rp. 45.000,-
+— VVIP HIJAB : Rp. 50.000,-
+— VVIP ONLYFANS : Rp. 50.000,-
+— VVIP CAMPURAN : Rp. 45.000,-
+— VVIP JAV HD : Rp. 40.000,-
+— VVIP LIVE RECORD : Rp. 45.000,-
 
-— VVIP PREMIUM : Rp. 100.000,-
+— VVIP PREMIUM : Rp. 115.000,-
 
 PROMO HEMAT 🪙
-— Rp. 165.000 TAKE ALL CHANNNEL VVIP NO PREMIUM
-— Rp. 225.000 TAKE ALL CHANNEL VVIP WITH PREMIUM
+— Rp. 250.000 TAKE ALL CHANNNEL VVIP NO PREMIUM
+— Rp. 335.000 TAKE ALL CHANNEL VVIP WITH PREMIUM
 
 PC @HeadSchool Kalau limit bisa pc bot @SchoolServiceBOT
             """,
